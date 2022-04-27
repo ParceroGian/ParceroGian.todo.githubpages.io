56e858932b61c82b4095ada1e8c40bcb2ef15ebc
Rafael Giandoso Silva <r.giandoso.silva@avanade.com>
2022-04-27 15:24:52 -0300

    Aula 3

---
+++ assets/js/todo.js
@@ -0,0 +1,40 @@
+console.log("Vai Corinthians!");
+
+// const - constante - nao muda. a nao ser objetos e referencia, ai o seu conteudo pode mudar
+
+const nome ="Rafael Giandoso";
+
+//let - variavel - pode mudar a quaquer momento.
+
+let email = "faelgiandoso@gmail.com";
+
+
+console.log(`Nome: ${nome} Email: ${email}`);
+
+let a = 10;
+let b = 0
+
+if (a > b)
+    {
+        console.log(a+b);
+    }
+
+    function escreve(x){
+        console.log(" Frase: " + x);
+    }
+
+    escreve(`Seja bem_vindo ${nome}`);
+    escreve("treinamento HTML%,CSS3 e JS");
+
+//##########################################################
+
+// objeto.metodo()
+// objeto.propriedade = valor;
+
+// document é um objeto.
+
+// window é um objetp.
+
+// qual propriedade, ou propriedade desse obejto.
+
+// window.innerheight window.innerwidth
