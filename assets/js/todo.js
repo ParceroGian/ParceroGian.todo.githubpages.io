783790d07e3259bdd03b3a440024ba8abcd78856
Rafael Giandoso Silva <r.giandoso.silva@avanade.com>
2022-05-04 07:50:02 -0300

    Aula 7

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,15 +1,15 @@
-import { addTask, getAllTasks} from "./utils/domFunctions.js";
+import { addTask, getAllTasks } from "./utils/domFunctions.js";
 
 
 //   // exemplo de array
 //   const frutas = ["maçã", "banana", "abacaxi", "abacate"];
-  
+
 //   // frutas[3];
 //   // frutas.length;
 //   // frutas[frutas.length - 1];
-  
+
 //   console.log(frutas.at(-1));
-  
+
 //   //TypeScript
 //   //Criando um objeto literal
 //   //JavaScript Object Notation - JSON
@@ -22,19 +22,19 @@ import { addTask, getAllTasks} from "./utils/domFunctions.js";
 //       console.log("Executando uma ação");
 //     },
 //   };
-  
+
 //   //Objeto é um conjuto de atributos(propriedades ou valores) e métodos(ações ou funçõs)
 //   console.log(dados.nome);
 //   console.log("Idade: ", dados.idade);
-  
+
 // dados.acao();
 // console.log(db[0].steps[2].step)
 // objeto é um conjunto de atributos(ou Propriedades) e métodos(ou funções ou ações)
- 
-  const db= 
+
+const db =
   [
-    { 
-      id: 1, 
+    {
+      id: 1,
       title: "Concluir App Conexão Arte",
       steps: [
         { step: "Ajustar textos" },
@@ -48,62 +48,77 @@ import { addTask, getAllTasks} from "./utils/domFunctions.js";
     },
 
     {
-        id: 2,
-        title: "CAula 4 Fiap - Avanade",
-        steps: [{ step: "Atributos Globais" }, { step: "Estrutura CSS" }],
-        done: true,
-        dueDate: "2022-05-06",
+      id: 2,
+      title: "Aula 4 Fiap - Avanade",
+      steps: [{ step: "Atributos Globais" }, { step: "Estrutura CSS" }],
+      done: true,
+      dueDate: "2022-05-06",
     },
 
   ];
 
 getAllTasks(db);
 
-  // console.log(db[0].title)
-  // DOM - Document Object Model - é o JS acessando o HTML e Manipulandos ele.
-  // HTML ele é compilado pelo navegador em uma arvore de comanddos, chama DOM.
+// console.log(db[0].title)
+// DOM - Document Object Model - é o JS acessando o HTML e Manipulandos ele.
+// HTML ele é compilado pelo navegador em uma arvore de comanddos, chama DOM.
 
 
-  const newTask = document.querySelector("#inputTxtNewTask");
+const newTask = document.querySelector("#inputTxtNewTask");
 
-  const form = document.querySelector('#addNewTask');
+const form = document.querySelector('#addNewTask');
 
-  form.addEventListener('submit', (e)=>
+form.addEventListener('submit', (e) => 
+{
+  e.preventDefault();
+});
+
+newTask.addEventListener('keyup', (e) => 
+{
+  e.preventDefault();
+  e.stopPropagation();
+  if (e.key == 'Enter') 
   {
-      e.preventDefault();
-  });
+    if (!newTask.value) 
+    {
+      alert("Digite uma nova tarefa");
+    }
+    else 
+    {
+      //alert(newTask.value);
 
-  newTask.addEventListener('keyup',(e)=>
-  {
-      e.preventDefault();
-      e.stopPropagation();
-      if(e.key == 'Enter')
+      const date = new Date();
+      const today = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+
+      // db.push([])
+      //   ({
+      //     id: Number(db.length) + 1,
+      //     title: newTask.value,
+      //     done: false,
+      //     dueDate: today,
+      //   });
+
+      const task = 
       {
-          if(!newTask.value)
-          {
-           alert("Digite uma nova tarefa");   
-          }
-          else
-          {
-            alert(newTask.value);
+        id: Number(db.length) + 1,
+        title: newTask.value,
+        done: false,
+        dueDate: today,
+      };
+      db.push(task);
 
-            const date = new Date();
-            const today = `${date.getDate()}/${date.getMonth() + 1 }/${date.getFullYear()}`;
-            db.push
-            ({
-                id: Number(db.length) +1,
-                title: newTask.value,
-                done: false,
-                dueDate: today,
-            });
-  
-            newTask.value = '';
-            
-            console.log(db);
-            console.log(newTask.value);
-          }
-        
+      addTask(task, newTask.value);
 
-      }
-  });
-  
+
+      //addTask(db, newTask.value);
+
+      // document.querySelector(".tasks").innerHTML='';
+      // getAllTasks(db);
+
+      // newTask.value = '';
+
+      console.log(db);
+      // console.log(newTask.value);
+    }
+  }
+});
--- assets/js/utils/domFunctions.js
+++ assets/js/utils/domFunctions.js
@@ -6,47 +6,60 @@
 //quando precisamos acessar uma função em outro arquivo, é necessário exportá-la.
 //CRUD - Create Read Update Delete 
 
-export function addTask(db, title='') {
-    //Criando uma nova tarefa
 
-    const task = document.createElement('div');
-    //const id = Number(db.lenght) +1;
-    task.classList.add('task');
-    task.setAttribute('id', db.id);
 
-    const taskBody =
+export function addTask(db, title = '') 
+{
+  //Criando uma nova tarefa
+
+  const task = document.createElement('div');
+  //const id = Number(db.lenght) +1;
+  task.classList.add('task');
+  task.setAttribute('id', db.id);
+
+  const taskBody =
     `
         <div> <input type="checkbox" id="check_${db.id}" /></div>
-            <div>
+          <div>
                 <div><span class="title-task">
-                ${title?title:db.title}</span></div> 
-            </div>
-            <div>Star</div>
+                ${title ? title : db.title}</span></div> 
+                <div>
+                    <ul>
+                      <br>
+                      <li class="dueDate">${db.dueDate}</li>
+                    </ul>
+                </div>
+          </div>
+        <div>Star</div>
     `;
 
-            // operador ternario. if de uma linha
-            // condicao? verdade : falso
+  // operador ternario. if de uma linha
+  // condicao? verdade : falso
 
 
-    task.innerHTML = taskBody;
-    document.querySelector('.tasks').appendChild(task);
+  task.innerHTML = taskBody;
+  document.querySelector('.tasks').appendChild(task);
 
-  }
-  export function removeTask() {
-    alert("Removendo uma nova tarefa");
-  }
-  export function updateTask() {
-    alert("Alterando uma nova tarefa");
-  }
-  export function getAllTasks(db) {
-      db.forEach(item => {
-        addTask(db);
-    });
-    
-  }
-  export function getTaskById() {
-    alert("Exibindo uma tarefa");
-  }
+}
+export function removeTask() 
+{
+  alert("Removendo uma nova tarefa");
+}
+export function updateTask() 
+{
+  alert("Alterando uma nova tarefa");
+}
+export function getAllTasks(db) 
+{
+  db.forEach(item => 
+  {
+    addTask(item);
+  });
+}
+export function getTaskById() 
+{
+  alert("Exibindo uma tarefa");
+}
   //quando for importar do arquivo domFunctions. o JS não sabe quantas funções estão disponíveis.
   // Quando eu preciso extrair qualquer recurso, usando a desestruturação. Destructuring
   //import { addTask } from './domFunctions.js';
